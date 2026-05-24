eaf92dc85c511c33ee4eae8fae4f0e3be3760098
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-05-24 09:01:09 +0530

    Update lineage_kebab.mk

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -13,9 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kebab
+PRODUCT_NAME := custom_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
