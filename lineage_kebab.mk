5f86c8618b590f4d47e5de9ca5ce369708f1ccc8
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-05-26 14:25:06 +0530

    Comment out common_full_phone.mk inheritance
    Comment out the inheritance of common full phone configuration.

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_kebab
 PRODUCT_DEVICE := kebab
