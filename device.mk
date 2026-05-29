7bd71aab84880c3836fe541e98e452ade91e2232
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-05-29 15:43:04 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -56,3 +56,6 @@ $(call inherit-product, device/oneplus/sm8250-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/kebab/kebab-vendor.mk)
+
+#Viper4fx
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
