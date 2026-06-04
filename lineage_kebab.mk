7cefacdac96132fbdbe5c3bd05bd6db70220858d
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-06-04 18:07:23 +0530

    Replace Mist config inheritance with Lineage config

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
-# Inherit some common Mist stuff.
-$(call inherit-product, vendor/Mist/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := mist_kebab
 PRODUCT_DEVICE := kebab
