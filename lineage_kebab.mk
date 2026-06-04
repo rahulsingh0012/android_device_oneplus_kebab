87b4a2265c800bcf1a8f3cabb64b36f48de03008
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-06-04 18:01:46 +0530

    Add files via upload

---
+++ lineage_kebab.mk
@@ -0,0 +1,61 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from kebab device
+$(call inherit-product, device/oneplus/kebab/device.mk)
+
+# Inherit some common Lunaris stuff.
+$(call inherit-product, vendor/lunaris/config/common_full_phone.mk)
+
+PRODUCT_NAME := lunaris_kebab
+PRODUCT_DEVICE := kebab
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := KB2005
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="OnePlus8T-user 14 UKQ1.230924.001 R.1a46c89-1_1 release-keys" \
+    BuildFingerprint=OnePlus/OnePlus8T/OnePlus8T:14/UKQ1.230924.001/R.1a46c89-1_1:user/release-keys \
+    DeviceName=OnePlus8T \
+    DeviceProduct=OnePlus8T \
+    SystemDevice=OnePlus8T \
+    SystemName=OnePlus8T
+
+# Device Flags
+
+WITH_GMS := true
+
+# Torch Control
+TORCH_STR_SUPPORTED := true
+
+# BLur
+TARGET_ENABLE_BLUR := true
+
+#UDFPS
+TARGET_HAS_UDFPS := true
+
+#xreality
+USE_REALITY_ENGINE := true
+
+#surfaceflinger
+$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
+
+#Touch boost
+SURFACE_FLINGER_BOOST := true
+
+# Charging
+BYPASS_CHARGE_SUPPORTED ?= true
+
+# Path for charge toggle
+BYPASS_CHARGE_TOGGLE_PATH ?= /sys/class/oplus_chg/battery/mmi_charging_enable
