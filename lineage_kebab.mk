96145553b42ccec4abaab595ec22a79476555aa5
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-06-05 09:33:49 +0530

    Update product inheritance in lineage_kebab.mk
    Replaced common Lineage inheritance with Matrixx inheritance.

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Matrixx stuff.
+$(call inherit-product, vendor/matrixx/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_kebab
 PRODUCT_DEVICE := kebab
