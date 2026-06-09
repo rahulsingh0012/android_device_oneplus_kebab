6f707c8f7fcc821a005b385a03242e228674d9c5
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-06-09 14:45:00 +0530

    Rename product from lineage_kebab to alpha_kebab

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/oneplus/kebab/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kebab
+PRODUCT_NAME := alpha_kebab
 PRODUCT_DEVICE := kebab
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
