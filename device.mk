f396880f9efecbf5b7476f2eeca9957d6dcd0358
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-06-11 14:08:53 +0530

    Add Soong namespace for QCOM WLAN

--- device.mk
+++ device.mk
@@ -47,6 +47,7 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+    hardware/qcom-caf/wlan
 
 # Touch
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
