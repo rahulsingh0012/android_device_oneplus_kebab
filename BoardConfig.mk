e7d4b33942f7b8c84dfc720394412660e094eb81
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-06-17 09:17:34 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,3 +33,8 @@ TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/kebab/BoardConfigVendor.mk
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
+# Infinity fixes
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom/wlan \
+    hardware/qcom-caf/wlan
