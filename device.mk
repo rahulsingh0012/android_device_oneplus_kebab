7918232b54e857d1a1005eb8c4dfc91beb31cf38
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-07-04 05:11:39 +0530

    Remove GameBar from device.mk
    Removed GameBar from product packages.

--- device.mk
+++ device.mk
@@ -27,9 +27,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.ramdisk
 
-#gamebar
-$(call inherit-product, packages/apps/GameBar/gamebar.mk)
-
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
