e0d10bd1bd7dba6e6cd71157f3f28105081d90f2
rahulsingh0012 <rahulrajsn35@gmail.com>
2026-07-11 09:44:47 +0530

    Add files via upload

---
+++ infinity_kebab.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from kebab device
+$(call inherit-product, device/oneplus/kebab/device.mk)
+
+# Inherit some common Infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+PRODUCT_NAME := infinity_kebab
+PRODUCT_DEVICE := kebab
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := KB2005
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="OnePlus8T-user 14 UKQ1.230924.001 R.1a46c89-1_1 release-keys" \
+    BuildFingerprint=OnePlus/OnePlus8T/OnePlus8T:14/UKQ1.230924.001/R.1a46c89-1_1:user/release-keys \
+    DeviceName=OnePlus8T \
+    DeviceProduct=OnePlus8T \
+    SystemDevice=OnePlus8T \
+    SystemName=OnePlus8T
+
+# Device Flags
+
+INFINITY_MAINTAINER := "Rahul_Singh"
+
+WITH_GAPPS := true
+
+# Torch Control
+TORCH_STR_SUPPORTED := true
+
+# BLur
+TARGET_ENABLE_BLUR := true
+
+#UDFPS
+TARGET_HAS_UDFPS := true
